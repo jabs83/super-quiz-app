51b599a2082e1adc99f14486bd5d2d0b8c0b2468
Jason <jasonb@dzap.net>
2014-03-23 23:50:08 -0700

    added more fixtures, updated score keep, and added last question method.

--- js/app.js
+++ js/app.js
@@ -14,13 +14,15 @@ App.QuizzesRoute = Ember.Route.extend({
   },
   actions: {
     answered: function(answer) {
-      this.controllerFor('quiz').set('currentScore', answer.weight);
-      alert(this.controllerFor('quiz').get('currentScore'));
+      var thisQuiz = this.controllerFor('quiz');
+      thisQuiz.set('currentScore', thisQuiz.get('currentScore') + answer.weight);
+      alert(thisQuiz.get('currentScore'));
     }
   }
 });
 
-App.QuizController = Ember.ObjectController.extend({});
+App.QuizController = Ember.ObjectController.extend({
+});
 
 App.QuizIndexRoute = Ember.Route.extend({
   model: function() {
@@ -28,19 +30,33 @@ App.QuizIndexRoute = Ember.Route.extend({
   }
 });
 
+App.QuestionController = Ember.ObjectController.extend({
+  needs:'quiz',
+  quiz: Ember.computed.alias("controllers.quiz"),
+  currentQuestion: function() {
+    if (this.get('content') === this.get('quiz').get('content.lastQuestion')){
+      return true;
+    }
+  }.property('this.content','controllers.quiz.content.lastQuestion')
+});
+
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Quiz = DS.Model.extend({
+  currentScore: 0,
   name: DS.attr('string'),
   description: DS.attr('string'),
   questions: DS.hasMany('question', {async: true}),
   firstQuestion: function() {
     return this.get('questions.firstObject');
-  }.property('questions.firstObject')
+  }.property('questions.firstObject'),
+  lastQuestion: function(){
+    return this.get('questions.lastObject');
+  }.property('questions.lastObject')
 });
 
 App.Question = DS.Model.extend({
   question: DS.attr('string'),
   quiz: DS.belongsTo('quiz', {async: true}),
-  answers: DS.attr('string')
+  answers: DS.attr('string'),
 });
--- js/fixtures.js
+++ js/fixtures.js
@@ -3,63 +3,70 @@ App.Quiz.FIXTURES = [
     id: 1,
     name: 'The JavaScript Quiz',
     description: 'take this quiz and find out if you\'re a super genious',
-    questions: [100,101]
+    questions: [100,101,102,103,104]
   },
   {
     id: 2,
     name: 'The Cascading Stylesheet Quiz',
     description: 'take this quiz and find out if you\'re a super genious in CSS',
-    questions: [102,103]
+    questions: [105,106]
   }
 ];
 
 App.Question.FIXTURES = [
       {
         id:100,
-        question: 'Inside which HTML element do we put the JavaScript',
-        quiz: 1,
+        question: '1. Inside which HTML element do we put the JavaScript?',
         answers: [
-          {
-            answer: 'alpha',
-            weight: 0
-          },
-          {
-            answer: 'beta',
-            weight: 5
-          },
-          {
-            answer: 'uno',
-            weight: 0
-          },
-          {
-            answer: 'dos',
-            weight: 5
-          }
+          { answer: '<js>', weight: 0.01 },
+          { answer: '<javascript>', weight: 0.01 },
+          { answer: '<scripting>', weight: 0.01 },
+          { answer: '<script>', weight: 5 }
         ]
       },
       {
         id:101,
-        question: 'How do you alert Hello World',
-        quiz: 1,
+        question: '2. What is the correct JavaScript syntax to write "Hello World"?',
         answers: [
-          {
-            answer: 'gamma',
-            weight: 0
-          },
-          {
-            answer: 'delta',
-            weight: 5
-          }
+          { answer: 'echo "Hello World";', weight: 0.01 },
+          { answer: 'document.write("Hello World");', weight: 5 },
+          { answer: '("Hello World");', weight: 0.01 },
+          { answer: 'response.write("Hello World");', weight: 0.01 }
         ]
       },
       {
         id:102,
+        question: '3. Where is the correct place to insert a JavaScript?',
+        answers: [
+          { answer: 'The <body> section', weight: 0.01 },
+          { answer: 'Both the <head> section and the <body> section are correct', weight: 5 },
+          { answer: 'The <head> section', weight: 0.01 }
+        ]
+      },
+      {
+        id:103,
+        question: '4. What is the correct syntax for referring to an external script called "xxx.js"?',
+        answers: [
+          { answer: '<script name="xxx.js">', weight: 0.01 },
+          { answer: '<script href="xxx.js">', weight: 0.01 },
+          { answer: '<script src="xxx.js">', weight: 5 }
+        ]
+      },
+      {
+        id:104,
+        question: '5. The external JavaScript file must contain the <script> tag.',
+        answers: [
+          { answer: 'True', weight: 0.01 },
+          { answer: 'False', weight: 5 }
+        ]
+      },
+      {
+        id:105,
         question: 'How do you declare an inline stylesheet?',
-        quiz: 2,
         answers: [
           {
             answer: 'alpha',
-            weight: 0
+            weight: 0.01
           },
           {
             answer: 'beta',
@@ -68,13 +75,12 @@ App.Question.FIXTURES = [
         ]
       },
       {
-        id:103,
+        id:106,
         question: 'Show how to include an external stylesheet',
-        quiz: 2,
         answers: [
           {
             answer: 'alpha',
-            weight: 0
+            weight: 0.01
           },
           {
             answer: 'beta',
